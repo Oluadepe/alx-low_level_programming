70927a5e0bb913274495b875605cd1f694964f8f
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-04-07 15:22:30 -0700

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 # include <stdio.h>
-
 /**
  * main - entry point 
  *
  * return: 0 after printing fuction
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle \n");
