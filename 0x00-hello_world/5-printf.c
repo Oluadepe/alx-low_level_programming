23d548a9d75c00aa2bac8b961a84a7ad7da43c70
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-04-07 16:23:56 -0700

    Task 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - entry point
+ * printf - output task
+ * Return 0 to complete task
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
