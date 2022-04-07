2705dbfea7a79fc55881431749e0bd047e74b08c
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-04-07 16:47:12 -0700

    Tasks 5-printf vim

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ * printf - ourput the data
+ * Retun - 1after code execute
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
