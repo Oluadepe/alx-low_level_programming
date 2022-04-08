1328df2a9e274bd5be07c736d9eb9454ae5e19ac
OLusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2022-04-08 23:45:19 +0000

    Task 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return: Always (Success)i
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
